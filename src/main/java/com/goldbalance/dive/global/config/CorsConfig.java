30e64149d3919a27ce851f0ab354e40a7e043b88
chosw1002@naver.com <chosw1002@naver.com>
2024-07-27 20:09:35 +0900

    feature: cors 세팅

---
+++ src/main/java/com/goldbalance/dive/global/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.goldbalance.dive.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:3000";
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(LOCAL_REACT_CLIENT_URL)
+			.allowedMethods("*");
+	}
+}
