c4bf4047949c7f4656dc3a1be3155fc0c92f9b06
chosw1002@naver.com <chosw1002@naver.com>
2024-07-27 18:10:21 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/goldbalance/dive/DiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldbalance.dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
