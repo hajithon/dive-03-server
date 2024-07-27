093800802e6cab2a5d470788dab085f3ec06907e
chosw1002@naver.com <chosw1002@naver.com>
2024-07-27 20:11:23 +0900

    fix: 포트 번호 수정

--- src/main/java/com/goldbalance/dive/global/config/CorsConfig.java
+++ src/main/java/com/goldbalance/dive/global/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-	private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:3000";
+	private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:5173";
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
